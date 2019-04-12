7ac4f7914b8f55b2c7ce423698d3db22488edf0a
xiangneng <pingan@pingan>
2019-04-12 18:08:23 +0800

    新建demo cloud

---
+++ cloudServer/src/main/java/org/cloudServer/CloudServerApplication.java
@@ -0,0 +1,13 @@
+package org.cloudServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudServerApplication {
+    public static void main( String[] args ){
+       SpringApplication.run(CloudServerApplication.class, args);
+    }
+}
